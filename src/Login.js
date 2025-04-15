4c5de2864f6f5be9a0ab019e0c0f5ca131ed3f7f
chong-hao <91341404@nfu.edu.tw>
2025-04-15 23:54:38 +0800

    1`

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { supabase } from './supabaseClient';
 
 function Login({ setCurrentUser }) {
   const [account, setAccount] = useState('');
